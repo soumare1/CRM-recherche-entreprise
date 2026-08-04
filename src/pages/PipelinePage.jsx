0b9ea40f321ba0db59dfe3404191b2f2cfcfac0a
soumare1 <soumare23.k@gmail.com>
2026-08-04 09:50:00 +0200

    Mise en place de la page Pipeline et chargement des opportunités

---
+++ src/pages/PipelinePage.jsx
@@ -0,0 +1,9 @@
+import PipelineBoard from '../components/pipeline/PipelineBoard';
+
+export default function PipelinePage() {
+  return (
+    <div className="h-full">
+      <PipelineBoard />
+    </div>
+  );
+}
